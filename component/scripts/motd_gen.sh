9a4c3f6dc72089587f070c1d718c49602d1b91a8
Simon Gerber <simon.gerber@vshn.ch>
2025-02-21 10:21:11 +0100

    Add ConsoleNotification `.spec.link` contents to motd

--- component/scripts/motd_gen.sh
+++ component/scripts/motd_gen.sh
@@ -3,7 +3,8 @@ set -exo pipefail
 
 jq 'del(.metadata) | .metadata.name = "motd" | .data.message = ([.data.message] + input | join("\n\n"))' \
     <(kubectl -n openshift get cm motd-template -ojson) \
-    <(kubectl get consolenotifications -l appuio.io/notification=true -ojson | jq '[.items[].spec.text]') \
+    <(kubectl get consolenotifications -l appuio.io/notification=true -ojson |
+      jq '[.items[]|.spec.text,if .spec.link.text then "\(.spec.link.text): \(.spec.link.href)" else "" end|select(length > 0)]') \
     > /tmp/motd.yaml
 
 if [ -z "$(jq -r '.data.message' /tmp/motd.yaml)" ]; then
